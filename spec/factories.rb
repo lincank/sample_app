4baf7ce5333ba412943a3746d37dd40272099f3c
Guimin Lin <lincank@gmail.com>
2012-08-03 21:53:30 +0800

    finish micropost

--- spec/factories.rb
+++ spec/factories.rb
@@ -17,4 +17,9 @@ FactoryGirl.define do
       admin true
     end
   end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
+  end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -99,6 +99,20 @@ describe "AuthenticationPages" do
         end
       end
 
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+      end
+
     end
 
     describe "as wrong user" do
